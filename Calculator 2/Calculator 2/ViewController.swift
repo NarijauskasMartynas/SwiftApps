7e7e146692caed0f8a383040bb3667ed01ceddb1
Martynas Narijauskas <martynasnarijauskas@Martynass-iMac.local>
2019-09-22 14:48:27 -0700

    Fixed dices layout, created layout for calculator

---
+++ Calculator 2/Calculator 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator 2
+//
+//  Created by Martynas Narijauskas on 9/22/19.
+//  Copyright © 2019 Martynas Narijauskas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
